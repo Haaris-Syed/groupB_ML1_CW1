fe176cd9bf4145851cfbfd6e6bd7ce7da3b72020
Michael Trent <mtrent133@gmail.com>
2023-02-09 23:12:52 +0000

    some skeleton code in classifier.py

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -5,14 +5,40 @@
 
 class Classifier:
     def __init__(self):
-        pass
+        self.decisionTree = DecisionTree()
 
     def reset(self):
         pass
     
     def fit(self, data, target):
-        pass
+        self.decisionTree.fit(data, target)
 
     def predict(self, data, legal=None):
         return 1
         
+class DecisionTree:
+    def __init__(self) -> None:
+        self.head = None
+
+    def fit(self, data, target):
+        pass
+
+    def predict(self):
+        pass
+
+class Node:
+    def __init__(self) -> None:
+        # pointers to child nodes
+        self.children = []
+        # feature index that we are splitting on
+        self.featureIndex = None
+
+    def predict(self):
+        pass
+
+class Leaf:
+    def __init__(self) -> None:
+        self.prediction = None
+
+    def predict(self):
+        return self.prediction
