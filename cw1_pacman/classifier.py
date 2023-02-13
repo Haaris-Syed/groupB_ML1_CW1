acd4c7c669e048222fbc03eea8e29e0e44f6eb5f
injunsuh <injun.suh@kcl.ac.uk>
2023-02-13 22:07:10 +0000

    implement gini impurity split

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -6,17 +6,53 @@ import numpy as np
 import random
 
 
+def featureIndexToSplitOn(features, data, target):
+    split_index = [gini(feature_index, data, target) for feature_index in range(len(features))]
+    min_index = split_index.index(min(split_index))
+
+    return min_index
+
+
+def gini(feature_index, data, target):
+    one, zero = [], []
+    target_counter = [[0] * 4, [0] * 4]
+
+    for i in list(zip(data, target)):
+        (one if int(i[0][feature_index]) else zero).append(i)
+        target_counter[int(i[0][feature_index])][int(i[1])] += 1
+
+    si = [len(one), len(zero)]
+    s = sum(si)
+    gini_impurity = 0
+
+    for i in range(2):
+        gi = sum([j ** 2 for j in target_counter[i]])
+        gi = 1 - (gi / (s ** 2))
+        gini_impurity += (si[i] / s) * gi
+
+    return gini_impurity
+
+
+def removeFeatureFromData(features, feature_index, data):
+    for i in range(2):
+        for j in range(len(data[i])):
+            data[i][j] = data[i][j][:feature_index] + data[i][j][feature_index + 1:]
+
+    features.pop(feature_index)
+
+    return features, data[0], data[1]
+
+
 class DecisionTree:
     def __init__(self):
         self.head = None
 
-    def fit(self, data, target, node=None, split_on=None):
-
+    def fit(self, data, target, node=None, features=None):
         # check if node is the root node
         if node is None:
             self.head = DecisionNode(data, None)
-            split_on = 0
             node = self.head
+            features = list(range(len(data[0])))
 
         # if there are no example in this branch then make it a leaf with its parents plurality value for class
         if len(data) == 0:
@@ -27,7 +63,7 @@ class DecisionTree:
         #     return Leaf(node.pluralityValue())
 
         # if there are no more features to split on
-        elif split_on == 24:
+        elif len(features) == 0:
             return LeafNode(node.pluralityValue())
 
         # if all the example have the same classification then make leaf with that classification
@@ -35,26 +71,26 @@ class DecisionTree:
             return LeafNode(target[0])
 
         else:
-            right = []
-            left = []
+            split_on = featureIndexToSplitOn(features, data, target)
+            rightData, rightTarget, leftData, leftTarget = [], [], [], []
+
+            node.featureIndex = features[split_on]
 
             # go through data and split on features --> binary split
             for i in list(zip(data, target)):
-                (right if int(i[0][split_on]) else left).append(i)
+                if int(i[0][split_on]):
+                    rightData.append(i[0])
+                    rightTarget.append(i[1])
+                else:
+                    leftData.append(i[0])
+                    leftTarget.append(i[1])
 
-            rightData, rightTarget, leftData, leftTarget = [], [], [], []
-
-            if len(right) != 0:
-                rightData, rightTarget = zip(*right)
-
-            if len(left) != 0:
-                leftData, leftTarget = zip(*left)
+            newFeatures, newRightData, newLeftData = removeFeatureFromData(features, split_on, [rightData, leftData])
 
             # recurse child nodes of current node incrementing which features to split on
-            node.right = self.fit(rightData, rightTarget, DecisionNode(rightTarget, node), split_on+1)
-            node.left = self.fit(leftData, leftTarget, DecisionNode(leftTarget, node), split_on+1)
-
-            node.featureIndex = split_on
+            # can change right_target to just right but do at end
+            node.right = self.fit(newRightData, rightTarget, DecisionNode(rightTarget, node), newFeatures)
+            node.left = self.fit(newLeftData, leftTarget, DecisionNode(leftTarget, node), newFeatures)
 
             return node
 
